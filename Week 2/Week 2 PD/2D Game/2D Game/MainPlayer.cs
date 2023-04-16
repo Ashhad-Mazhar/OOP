0c2014f0ffb31b76b2c0dd10ba94a9216adbc911
Ashhad-Mazhar <113442315+Ashhad-Mazhar@users.noreply.github.com>
2023-04-16 23:37:34 +0500

    Add files via upload

---
+++ Week 2/Week 2 PD/2D Game/2D Game/MainPlayer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2D_Game
+{
+    class MainPlayer
+    {
+        public int mainPlayerX;
+        public int mainPlayerY;
+        public int playerHealth;
+        public int bulletsRemaining;
+        public int score;
+    }
+}
