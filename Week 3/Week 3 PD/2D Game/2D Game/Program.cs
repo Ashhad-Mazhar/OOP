930d6979bdcefb14d732872a526ba36470ff4c1a
Ashhad-Mazhar <113442315+Ashhad-Mazhar@users.noreply.github.com>
2023-05-01 23:35:07 +0500

    Add files via upload

---
+++ Week 3/Week 3 PD/2D Game/2D Game/Program.cs
@@ -0,0 +1,319 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EZInput;
+using System.Threading;
+
+namespace _2D_Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[,] mainPlayer = new char[,]
+            {
+                { ' ', '_', '_', '_', ' ', ' ' },
+                { '(', 'O', 'v', 'O', ')', ' ' },
+                { ' ', '|', ' ', '|', '-', '>' },
+                { ' ', '|', '_', '|', ' ', ' ' }
+            };
+
+            char[,] enemyA = new char[,]
+            {
+                { ' ', ' ', '_', '_', '_', ' ' },
+                { ' ', '(', 'O', 'v', 'O', ')' },
+                { '<', '-', '|', ' ', '|', ' ' },
+                { ' ', ' ', '|', '_', '|', ' ' }
+            };
+
+            MainPlayer s = new MainPlayer();
+            Enemy EnemyA = new Enemy();
+
+            int timer = 0;
+
+            char[,] maze = new char[47, 142];
+
+            ReadData(maze);
+            PrintMaze(maze);
+            PrintPlayer(maze, ref s, mainPlayer);
+            while (true)
+            {
+                PrintScore(ref s.score);
+                PrintBulletsRemaining(ref s.bulletsRemaining);
+                PrintPlayerHealth(ref s.playerHealth);
+                Console.SetCursorPosition(0, 0);
+                PrintMaze(maze);
+                if (Keyboard.IsKeyPressed(Key.LeftArrow))
+                {
+                    MovePlayerLeft(maze, ref s, mainPlayer);
+                }
+                if (Keyboard.IsKeyPressed(Key.RightArrow))
+                {
+                    MovePlayerRight(maze, ref s, mainPlayer);
+                }
+                if (Keyboard.IsKeyPressed(Key.UpArrow))
+                {
+                    MovePlayerUp(maze, ref s, mainPlayer);
+                }
+                if (Keyboard.IsKeyPressed(Key.DownArrow))
+                {
+                    MovePlayerDown(maze, ref s, mainPlayer);
+                }
+
+                if (timer % 3 == 0)
+                {
+                    MoveEnemyA(maze, ref EnemyA, enemyA);
+                }
+                timer++;
+                Thread.Sleep(1);
+                if (s.playerHealth <= 0)
+                {
+                    GameOverScreen(ref s.score);
+                    break;
+                }
+            }
+        }
+
+        static void PrintScore(ref int score)
+        {
+            //Used to print the player's curent score
+            Console.SetCursorPosition(65, 94);
+            Console.Write("Score: " + score);
+        }
+
+        static void MoveEnemyA(char[,] maze, ref Enemy EnemyA, char[,] enemy)
+        {
+            // Used to change enemy's position
+            if (EnemyA.EnemyDirection == "Up")
+            {
+                if (maze[EnemyA.EnemyY - 1, EnemyA.EnemyX] == ' ')
+                {
+                    EraseEnemy(maze, ref EnemyA);
+                    EnemyA.EnemyY--;
+                    PrintEnemy(maze, ref EnemyA, enemy);
+                }
+                if (maze[EnemyA.EnemyY - 1, EnemyA.EnemyX] == '#')
+                {
+                    EnemyA.EnemyDirection = "Down";
+                }
+            }
+            if (EnemyA.EnemyDirection == "Down")
+            {
+                if (maze[EnemyA.EnemyY + 4, EnemyA.EnemyX] == ' ')
+                {
+                    EraseEnemy(maze, ref EnemyA);
+                    EnemyA.EnemyY++;
+                    PrintEnemy(maze, ref EnemyA, enemy);
+                }
+                if (maze[EnemyA.EnemyY + 4, EnemyA.EnemyX] == '#')
+                {
+                    EnemyA.EnemyDirection = "Up";
+                }
+            }
+        }
+
+        static void PrintEnemy(char[,] maze, ref Enemy EnemyA, char[,] enemy)
+        {
+            // Used to display enemy on the screen
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 6; j++)
+                {
+                    maze[EnemyA.EnemyY + i, EnemyA.EnemyX + j] = enemy[i, j];
+                }
+            }
+        }
+
+        static void EraseEnemy(char[,] maze, ref Enemy EnemyA)
+        {
+            // Used to erase the enemy from the screen
+            for (int i = 0; i < 4; i++)
+            {
+                for (int index = 0; index < 6; index++)
+                {
+                    maze[EnemyA.EnemyY + i, EnemyA.EnemyX + index] = ' ';
+                }
+            }
+        }
+
+        static void MovePlayerUp(char[,] maze, ref MainPlayer s, char[,] mainPlayer)
+        {
+            // Used to move the player one space upward
+            if (maze[s.mainPlayerY - 1, s.mainPlayerX] == ' ')
+            {
+                ErasePlayer(maze, ref s);
+                s.DecrementMainPlayerY();
+                PrintPlayer(maze, ref s, mainPlayer);
+            }
+        }
+        static void MovePlayerDown(char[,] maze, ref MainPlayer s, char[,] mainPlayer)
+        {
+            // Used to move the player one space downward
+            if (maze[s.mainPlayerY + 1, s.mainPlayerX] == ' ')
+            {
+                ErasePlayer(maze, ref s);
+                s.IncrementMainPlayerY();
+                PrintPlayer(maze, ref s, mainPlayer);
+            }
+        }
+        static void MovePlayerLeft(char[,] maze, ref MainPlayer s, char[,] mainPlayer)
+        {
+            // Used to move the player one space left
+            if (maze[s.mainPlayerY, s.mainPlayerX - 1] == ' ')
+            {
+                ErasePlayer(maze, ref s);
+                s.DecrementMainPlayerX();
+                PrintPlayer(maze, ref s, mainPlayer);
+            }
+        }
+        static void MovePlayerRight(char[,] maze, ref MainPlayer s, char[,] mainPlayer)
+        {
+            // Used to move the player one space right
+            if (maze[s.mainPlayerX + 1, s.mainPlayerY] == ' ')
+            {
+                ErasePlayer(maze, ref s);
+                s.IncrementMainPlayerX();
+                PrintPlayer(maze, ref s, mainPlayer);
+            }
+        }
+
+        static void PrintPlayer(char[,] maze, ref MainPlayer s, char[,] mainPlayer)
+        {
+            // Used to display the player on the screen
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 6; j++)
+                {
+                    maze[s.mainPlayerY + i, s.mainPlayerX + j] = mainPlayer[i, j];
+                }
+            }
+        }
+
+        static void ErasePlayer(char[,] maze, ref MainPlayer s)
+        {
+            // Used to erase the player from the screen
+            for (int i = 0; i < 4; i++)
+            {
+                for (int index = 0; index < 6; index++)
+                {
+                    maze[s.mainPlayerY + i, s.mainPlayerX + index] = ' ';
+                }
+            }
+        }
+
+        static void PrintMaze(char[,] maze)
+        {
+            for (int x = 0; x < maze.GetLength(0); x++)
+            {
+                for (int y = 0; y < maze.GetLength(1); y++)
+                {
+                    Console.Write(maze[x, y]);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static void ReadData(char[,] maze)
+        {
+            StreamReader fp = new StreamReader("maze.txt");
+            string record;
+            int row = 0;
+            while ((record = fp.ReadLine()) != null)
+            {
+                for (int x = 0; x < 142; x++)
+                {
+                    maze[row, x] = record[x];
+                }
+                row++;
+            }
+            fp.Close();
+        }
+
+        static void PrintBulletsRemaining(ref int bulletsRemaining)
+        {
+            // Used to display the number of bullets remaining
+            Console.SetCursorPosition(65, 96);
+            Console.Write("Bullets:          ");
+            Console.SetCursorPosition(65, 96);
+            Console.Write("Bullets: " + bulletsRemaining);
+        }
+
+        static void PrintPlayerHealth(ref int playerHealth)
+        {
+            // Used to display the player's health
+            Console.SetCursorPosition(65, 98);
+            Console.Write("Health:           ");
+            Console.SetCursorPosition(65, 98);
+            Console.Write("Health: " + playerHealth);
+        }
+
+        static void GameOverScreen(ref int score)
+        {
+            // Used to print the game over screen
+            Console.Clear();
+            Console.WriteLine("      _____                  ");
+            Console.WriteLine("     / ___/ ___ _  __ _  ___ ");
+            Console.WriteLine("    / (_ / / _ `/ /  ' \\/ -_)");
+            Console.WriteLine("    \\___/  \\_,_/ /_/_/_/\\__/ ");
+            Console.WriteLine(" ");
+            Console.WriteLine("      ____                   ");
+            Console.WriteLine("     / __ \\ _  __ ___   ____ ");
+            Console.WriteLine("    / /_/ /| |/ // -_) / __/ ");
+            Console.WriteLine("    \\____/ |___/ \\__/ /_/    ");
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Console.WriteLine("     Your score: " + score);
+            Console.WriteLine(" ");
+            Console.WriteLine("Press any key to continue");
+            Console.ReadKey();
+        }
+        class MainPlayer
+        {
+            public int mainPlayerX;
+            public int mainPlayerY;
+            public int playerHealth;
+            public int bulletsRemaining;
+            public int score;
+            public MainPlayer()
+            {
+                mainPlayerX = 10;
+                mainPlayerY = 42;
+                playerHealth = 100;
+                score = 0;
+                bulletsRemaining = 100;
+            }
+            public void DecrementMainPlayerY()
+            {
+                mainPlayerY--;
+            }
+            public void IncrementMainPlayerY()
+            {
+                mainPlayerY++;
+            }
+            public void DecrementMainPlayerX()
+            {
+                mainPlayerX--;
+            }
+            public void IncrementMainPlayerX()
+            {
+                mainPlayerX++;
+            }
+        }
+
+        class Enemy
+        {
+            public int EnemyX;
+            public int EnemyY;
+            public string EnemyDirection;
+            public Enemy()
+            {
+                EnemyX = 30;
+                EnemyY = 10;
+                EnemyDirection = "Up";
+            }
+        }
+    }
+}
---
+++ Week 3/Week 3 PD/Business Application/Business Application/Program.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business_Application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "Users.txt";
+            int currentUsers = 0;
+            List<Credentials> s = new List<Credentials>();
+            currentUsers = LoadData(s, path);
+            int input = 0;
+            while (input != 5)
+            {
+                PrintNewScreen("Menu Screen");
+                Console.WriteLine("1. Sign Up");
+                Console.WriteLine("2. Sign In");
+                Console.WriteLine("3. Update");
+                Console.WriteLine("4. Delete");
+                Console.WriteLine("5. Quit");
+                Console.WriteLine("Enter your choice: ");
+                input = int.Parse(Console.ReadLine());
+                if (input == 1)
+                {
+                    PrintNewScreen("Sign Up");
+                    currentUsers = SignUp(s, path, currentUsers);
+                }
+                else if (input == 2)
+                {
+                    PrintNewScreen("Sign In");
+                    SignIn(s, path, currentUsers);
+                }
+                else if (input == 3)
+                {
+                    PrintNewScreen("Update Record");
+                    UpdateRecord(s, path, currentUsers);
+                }
+                else if (input == 4)
+                {
+                    PrintNewScreen("Delete Record");
+                    currentUsers = DeleteRecord(s, path, currentUsers);
+                }
+            }
+        }
+        static int SignUp(List<Credentials> s, string path, int currentUsers)
+        {
+            string Record = "";
+            Credentials info = new Credentials();
+            info.InputCredentials();
+            s.Add(info);
+            currentUsers++;
+            Record = info.username + "," + info.password;
+            StreamWriter filevar = new StreamWriter(path, true);
+            filevar.WriteLine(Record);
+            filevar.Flush();
+            filevar.Close();
+            Console.WriteLine("User successfully registered");
+            Console.WriteLine("Press any key to continue");
+            Console.ReadKey();
+            return currentUsers;
+        }
+        static void SignIn(List<Credentials> s, string path, int currentUsers)
+        {
+            string n, p;
+            Console.WriteLine("Enter username: ");
+            n = Console.ReadLine();
+            Console.WriteLine("Enter password: ");
+            p = Console.ReadLine();
+            for (int i = 0; i < currentUsers; i++)
+            {
+                if (n == s[i].username && p == s[i].password)
+                {
+                    Console.WriteLine("User successfully logged in");
+                    Console.WriteLine("Press any key to continue");
+                    Console.ReadKey();
+                    break;
+                }
+            }
+        }
+        static void UpdateRecord(List<Credentials> s, string path, int currentUsers)
+        {
+            string Username, Password;
+            Console.WriteLine("Enter the username of the record you want to update: ");
+            Username = Console.ReadLine();
+            Console.WriteLine("Enter the password of the record you want to update: ");
+            Password = Console.ReadLine();
+            for (int i = 0; i < currentUsers; i++)
+            {
+                if (Username == s[i].username && Password == s[i].password)
+                {
+                    s[i].InputCredentials();
+                    RewriteData(s, path, currentUsers);
+                    Console.WriteLine("User successfully updated");
+                    Console.WriteLine("Press any key to continue");
+                    Console.ReadKey();
+                }
+            }
+        }
+        static void RewriteData(List<Credentials> s, string path, int currentUsers)
+        {
+            string Record = "";
+            StreamWriter file = new StreamWriter(path);
+            for (int i = 0; i < currentUsers; i++)
+            {
+                Record = s[i].username + "," + s[i].password;
+                file.WriteLine(Record);
+            }
+            file.Flush();
+            file.Close();
+        }
+        static int DeleteRecord(List<Credentials> s, string path, int currentUsers)
+        {
+            string n, p;
+            Console.WriteLine("Enter the username of the record you want to delete: ");
+            n = Console.ReadLine();
+            Console.WriteLine("Enter the password of the record you want to delete: ");
+            p = Console.ReadLine();
+            for (int i = 0; i < s.Count; i++)
+            {
+                if (n == s[i].username && p == s[i].password)
+                {
+                    s.RemoveAt(i);
+                    currentUsers--;
+                    RewriteData(s, path, currentUsers);
+                    Console.WriteLine("User successfully deleted");
+                    Console.WriteLine("Press any key to continue");
+                    Console.ReadKey();
+                    break;
+                }
+            }
+            return currentUsers;
+        }
+        static int LoadData(List<Credentials> s, string path)
+        {
+            int currentUsers = 0;
+            string record = "";
+            StreamReader file = new StreamReader(path);
+            while ((record = file.ReadLine()) != null)
+            {
+                Credentials info = new Credentials();
+                info.username = Parsing(record, 1);
+                info.password = Parsing(record, 2);
+                s.Add(info);
+                currentUsers++;
+            }
+            file.Close();
+            return currentUsers;
+        }
+        static string Parsing(string record, int field)
+        {
+            string result = "";
+            int comma = 1;
+            for (int i = 0; i < record.Length; i++)
+            {
+                if (comma == field)
+                {
+                    result = result + record[i];
+                }
+                else if (record[i] == ',')
+                {
+                    comma++;
+                }
+            }
+            return result;
+        }
+        static void PrintHeader()
+        {
+            // Used to print the header of the application.
+            Console.Write("\n");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("************************************************************************************************************************");
+            Console.Write("\n");
+            Console.Write("********************************************** ");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("Hospital Management System ");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("**********************************************");
+            Console.Write("\n");
+            Console.Write("************************************************************************************************************************");
+            Console.Write("\n\n");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        static void PrintSubHeader(string heading)
+        {
+            // Used to print the subheader of the application.
+            Console.Write(heading);
+            Console.Write("\n");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("------------------------------------------------------------------------------------------------------------------------");
+            Console.Write("\n\n");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        static void PrintNewScreen(string subHeader)
+        {
+            // Used to print the header and subheader of a new screen.
+            Console.Clear();
+            PrintHeader();
+            PrintSubHeader(subHeader);
+        }
+    }
+    class Credentials
+    {
+        public string username;
+        public string password;
+        public Credentials()
+        {
+            username = "";
+            password = "";
+        }
+        public Credentials(string Name, string Password)
+        {
+            username = Name;
+            password = Password;
+        }
+        public void InputCredentials()
+        {
+            Console.WriteLine("Enter new username: ");
+            username = Console.ReadLine();
+            Console.WriteLine("Enter new password: ");
+            password = Console.ReadLine();
+        }
+    }
+}
